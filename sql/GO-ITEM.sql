f0a640c3966e582227c7e90e85318a6f26c570b6
Grumbo <blackwolfsden@hotmail.com>
2015-07-30 17:13:48 -0600

    Update and rename Black_Vendor.sql to GO-ITEM.sql

---
+++ sql/GO-ITEM.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `equipment_id`, `mg`)
+VALUES (390001,0,0,0,0,0,31048,0,0,0, 'Grumbo\'z Black Jack', 'and hookerz', 'Buy',0,80,80,35,129,1,1.14286,3.5, 0, 0);
+
+REPLACE INTO `item_template` (`entry`, `class`, `subclass`, `SoundOverrideSubclass`, `name`, `displayid`, `Quality`, `Flags`, `FlagsExtra`, `BuyCount`, `AllowableClass`, `AllowableRace`, `ItemLevel`, `stackable`, `ContainerSlots`, `StatsCount`, `bonding`, `description`, `BagFamily`) VALUES 
+(44209,10,0,-1, 'Magic Gold',32282,1,64,0,1,-1,-1,0,2147483647,0,0,0, 'custom currency',8192);
